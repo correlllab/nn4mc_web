161da06630cde349c90516c36671b3fc849ee996
LeeLee Graham <graham.la3@gmail.com>
2020-04-06 01:51:44 -0600

    Implemented the hidden element for cards and loading gif

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -2,18 +2,22 @@ var $inputTitle = $('#input-title');
 var $inputType = $('#input-type');
 var $chosenFile = $('#choose-file');
 var $submitButton = $('#submit-button');
-var $outputAlbumArea = $('main');
+var $outputAlbumArea = $('.card-box');
 var $outputTitle = $('h2');
 var $outputType = $('p');
 var $uploadedFile = $('#new-file');
 var $deleteButton = $('#delete-button');
-var $loveButton = $('#love-button')
+var $loveButton = $('#love-button');
+var $loader = $('.ld');
+var $cards = $('article');
+var $download = $('a');
 
 
 $inputTitle.on('keyup', submitButtonToggle);
 $inputType.on('keyup', submitButtonToggle);
 $chosenFile.on('change', submitButtonToggle)
-$submitButton.on('click', uploadCard);
+$submitButton.on('click', toggle_ld);
+$download.click(fireDownload);
 
 function submitButtonToggle() {
   if (($inputTitle.val() && $inputType.val()) && $chosenFile.val()) {
@@ -23,8 +27,9 @@ function submitButtonToggle() {
   }
 }
 
-function uploadCard(event) {
+function uploadCard(event, resp) {
   event.preventDefault();
+  // console.log(resp); //<---making sure the response is passed
   if($submitButton.is(':disabled')){
     alert('Please enter all form fields to');
   }
@@ -54,32 +59,34 @@ function clearInputs() {
 //COOPER
 //This function will five the file data to the python server
 //Not entirely sure about the "function(err...)" part
-//Need some kind of loading screen
-function translateFile()
-{
-  var data = 1;//Get file upload from html
 
+// LEELEE
+//** err is an error message built in that can be used in the event the request isn't successful **
+function translateFile(event) {
+  event.preventDefault();
+  var data = 1;//Get file upload from html
   $.post("/postdata",
   {file_data: data},
   function(err, req, resp){
-    //Just for now so you see loading screen.
-    //setTimeout(function(){ toggle_ld(); }, 3000);
-
     //Update data in html page (NOTE: Not gonna redirect)
-    console.log(resp.responseText);
-    
+    // passing the response text to uploadCard, along with the event, for checks
+    console.log(resp, req, err);
+    uploadCard(event, resp);    
   });
 }
 
-function toggle_ld()
-{
-  document.getElementById('submit-button').classList.toggle('running');
+function toggle_ld(event) {
+  event.preventDefault();
+  $loader.prop('hidden', false);
+  $outputAlbumArea.prop('hidden', true)
+  setTimeout(function() {$loader.prop('hidden', true)}, 3000);
+  setTimeout(function() {uploadCard(event)}, 3000);
+  setTimeout(function() {$outputAlbumArea.prop('hidden', false)}, 3000);
+  $download.prop('hidden', false);
 }
 
-$("#submit-button").click(function(){
-    toggle_ld();
-    translateFile();
-  });
+function fireDownload(event) {
+    event.preventDefault();  
+    window.location.href = 'uploads/file.doc';  
+}
 
-
-// document.getElementById("yourFileInput").files[0].fileName;
