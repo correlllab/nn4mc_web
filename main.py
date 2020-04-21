3b0e1c5c4cafe55fb3c3d9e00abe40ab9d565577
RS-Coop <simpson.r.cooper@gmail.com>
2020-04-21 09:51:40 -0600

    Fleshing out some of the server processing.

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request
 import os
 import json
-# import nn4mc
+import io
+import nn4mc.Translator as nnTR
 
 app = Flask(__name__)
 
@@ -13,12 +14,14 @@ def dir_last_updated(folder):
                    for root_path, dirs, files in os.walk(folder)
                    for f in files))
 
+################################################################################
+#Page serving
+
 #Loads homepage
 @app.route('/', methods=['GET'])
 def homepage():
     return render_template('homepage.html',
                             last_updated=dir_last_updated('static'))
-
 
 #Loads documentation page
 @app.route('/', methods=['GET'])
@@ -26,27 +29,38 @@ def documentation():
     return render_template('documentation.html',
                             last_updated=dir_last_updated('static'))
 
+################################################################################
+#Get and Post
 
 #Used by JS to send file data to python for processing
+#NOTE: File is coming in as string, is converted to bytes, and
+#resultant files are returned as JSON
 @app.route('/postdata', methods=['POST'])
 def post_file_data():
-    file = request.form['file_data']
+    infile = request.form['file_data'] #Get the data from JS
+    # infile = json.loads(file)
 
-    #Process the file here and then send back other data
-    files = json.dumps('hi')
+    #Extract file type and data
+    type = infile["type"]
+    data = infile["text"]
 
-    return files
+    #Encode file as bytes and convert to BytesIO object
+    byt = data.encode('utf-8')
+    file_obj = io.BytesIO(byt)
+
+    files = nnTr.translateToJSON(file_obj, type) #Translate the file
+
+    return files #Return JSON output
 
 #Function for testing file process
 @app.route('/posttest', methods=['GET'])
 def post_test_data():
-    #Use nn4mc to process file
-
     with open('test_data/sample.json') as json_file:
         files = json.load(json_file)
 
     return files
 
+################################################################################
 #Currently not a thing, could be used for graph visualization
 @app.route('/plot/<imgdata>')
 def plot(imgdata):
