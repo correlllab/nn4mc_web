3538a3b4e9de26e9e5fda7a3991d2f6db9035bae
sarahaguasvivas <saag5228@colorado.edu>
2022-01-11 12:40:42 -0700

    updating flask app

--- config.py
+++ config.py
@@ -1,2 +1,6 @@
+from google.appengine.ext import vendor
+
 DEBUG = False #Make sure to set to false in production
 SEND_FILE_MAX_AGE_DEFAULT = 0 #Lame fix but not sure what else to do
+
+vendor.add('flask')
